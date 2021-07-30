d2063ffa15ca6b4643cfb958f51d974e08b70b30
Naman Mishra <kayyshu@gmail.com>
2021-07-30 14:21:24 +0530

    Module 1 30 July- RELEASED

--- src/custom/functions/PropertyFromCSV.java
+++ src/custom/functions/PropertyFromCSV.java
@@ -9,31 +9,18 @@ import java.util.Scanner;
 
 public class PropertyFromCSV {
     public ArrayList<Property> processCSVToPropertyList(String filePath) throws FileNotFoundException {
-        ArrayList < Property > resultList = new ArrayList < Property > ();
+        ArrayList < Property > resultList = new ArrayList <> ();
         Scanner scan = new Scanner(new File(filePath));
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
             String[] lineArray = line.split(",");
             resultList.add(new Property(Integer.parseInt(lineArray[0]), Integer.parseInt(lineArray[1]), lineArray[2], Integer.parseInt(lineArray[3]), Integer.parseInt(lineArray[4]),Integer.parseInt(lineArray[5]),lineArray[6],Integer.parseInt(lineArray[7]),Integer.parseInt(lineArray[8])));
         }
-
-//        System.out.println("PropertyID PropertyType PropertySpace Bathrooms Bedrooms BuildingSpace LandSpace Street getStreetNumber");
-//        for(Property property: resultList){
-//            System.out.print(property.getPropertyID()+" ");
-//            System.out.print(property.getPropertyType()+" ");
-//            System.out.print(property.getPropertySpace()+" ");
-//            System.out.print(property.getBathrooms()+" ");
-//            System.out.print(property.getBedrooms()+" ");
-//            System.out.print(property.getBuildingSpace()+" ");
-//            System.out.print(property.getLandSpace()+" ");
-//            System.out.print(property.getStreet()+" ");
-//            System.out.println(property.getStreetNumber());
-//        }
         return resultList;
     }
 
     public Integer getNumberOfProperties(String filePath) throws FileNotFoundException {
-        ArrayList < Property > resultList = new ArrayList < Property > ();
+        ArrayList < Property > resultList = new ArrayList <> ();
         Scanner scan = new Scanner(new File(filePath));
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
@@ -42,6 +29,5 @@ public class PropertyFromCSV {
         }
         return resultList.size();
     }
-
 
 }
